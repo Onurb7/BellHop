582ea36ae7345e23f85254095a3ff22931d85942
Onurb7 <kontrec.bruno@gmail.com>
2026-07-11 23:06:59 +0200

    git ignores added

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    ...(class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)
+        ? [App\Providers\TelescopeServiceProvider::class]
+        : []),
 ];
